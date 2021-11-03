3eb8d765cd3aac9f0a3bc00d5c005582bd3f564e
Antoine07 <antoine.lucsko@gmail.com>
2021-11-03 17:03:43 +0100

    event manager

--- 05_DESIGN_PATTERNS/Patterns/Behavioral/Exercices/08_Event/tests/EventManagerTest.php
+++ 05_DESIGN_PATTERNS/Patterns/Behavioral/Exercices/08_Event/tests/EventManagerTest.php
@@ -2,27 +2,43 @@
 
 use App\User;
 use App\FactoryPDO;
+use App\EventManager;
 
 use PHPUnit\Framework\TestCase;
 
 class EventTest extends TestCase
 {
     protected User $user;
+    protected EventManager $eventManager;
 
     public function setUp(): void
     {
         $m = new Migration();
         $pdo = FactoryPDO::buildSqlite("sqlite:" . __DIR__ . "/_data/database.db");
         $m->setData($pdo);
+
+        $this->user = new User("sqlite:" . __DIR__ . "/_data/database.db");
+
+        $this->eventManager = new EventManager();
     }
 
     public function testFirstUser()
     {
-    
+        $user = $this->user->find(1);
+
+        $this->assertEquals($user->getId(), 1);
+        $this->assertEquals($user->address, "Paris");
     }
 
     public function testAllUsers()
     {
-        
+        $users = $this->user->all();
+
+        $this->assertEquals(count($users), 30);
+    }
+
+    public function testEventManager(){
+
+        $this->assertInstanceOf(EventManager::class, $this->eventManager);
     }
 }
